606c683f6b43685f2ea050894a54db3904a71052
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2018-04-17 17:50:50 +0200

    end 17/04/18

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function carruselLogIn(){
+    var fotos = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg"];
+    var img=document.getElementById("carruselLogIn")
+    img.src=fotos[Math.floor(Math.random()* 4)]
+    
+}
