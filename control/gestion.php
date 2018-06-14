7e008e521e020c11990761cb3c60d853f917c2c9
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2018-06-14 10:15:14 +0200

    antes de presentar ultima tutoria

--- control/gestion.php
+++ control/gestion.php
@@ -18,11 +18,50 @@ session_start();
         <h1 class="display-4">Hola,
             <?php echo $_SESSION["nombre"];?>
         </h1>
-        <p class="lead">Edita tu informacion o date de baja si asi lo deseas</p>
+        <p class="lead">Edita la informacion que eligas o date de baja si asi lo deseas</p>
     </div><br>
     <div class="container">
 
-        
+     
+    <div class="container">
+            <br><br>
+            <form class="form-horizontal" name="formregistro" id="formregistro" action="enviarRegistro.php" method="post">
+                <div class="row form-group">
+                    <div class="col-sm-3">
+                        <input type="text" name="nombre" class="form-control" id="nombre" placeholder="Nombre" required>
+                    </div>
+                    <div class="col-sm-3">
+                        <input type="text" name="apellido" class="form-control" id="apellido" placeholder="Apellido" required>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-sm-3">
+                        <input type="password" name="pwd" class="form-control" id="pwd" placeholder="Contraseña" required>
+                    </div>
+                    <div class="col-sm-3">
+                        <input type="password" name="pwddos" class="form-control" id="pwddos" placeholder="Repita la contraseña" required>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-sm-3">
+                        <input type="email" name="email" class="form-control" id="email" placeholder="Correo Electronico" required>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-sm-3">
+                        <input placeholder="Fecha de nacimiento" class="form-control" type="text" onfocus="(this.type='date')" id="fechanacimiento" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" onclick="comprobarRegistro()" class="btn btn-default">Cambiar</button>
+                        <button type="submit" onclick="comprobarRegistro()" class="btn btn-danger">Eliminar cuenta</button>
+
+                    </div>
+                </div>
+            </form>
+        </div>    
+              
     </div>
     <div class="navbar navbar-expand-sm bg-dark navbar-dark navbar-fixed-bottom" style="position:fixed;bottom:0;width: 100%">
         <div class="container">
